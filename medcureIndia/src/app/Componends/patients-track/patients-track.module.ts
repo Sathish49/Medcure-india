b51638343343206d4f330dc645641364b6461631
Sathish49 <penugondasathish49@gmail.com>
2020-11-12 15:19:05 +0530

    Implemented home page navigation

---
+++ medcureIndia/src/app/Componends/patients-track/patients-track.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PatientsTrackRoutingModule } from './patients-track-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PatientsTrackRoutingModule
+  ]
+})
+export class PatientsTrackModule { }
